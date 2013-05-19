022ff942c202e1c2af7e6167c9a9daee2785c0d5
Paweł Marynowski <yarl@o2.pl>
2013-05-19 19:33:12 +0200

    dodane pozostałe punkty

--- info.js
+++ info.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
     
     mapa.dodaj(
+        [51.2800849, 17.9895302],
+        "Synagoga (ul. Łazienkowa 6)",
+        "Została wybudowana w latach 1815-1816 według projektu architektów z Brzegu, braci Fryderyka i Karola Schefflerów. Stanęła w miejscu starszej, drewnianej bóżnicy. Murowany budynek  wzniesiono na planie prostokąta w stylu klasycystycznym. Fasada gmachu ze wspaniałym, monumentalnym portykiem toskańskim nawiązuje do fasady opery w Berlinie. W świątyni przeprowadzono dwukrotnie remont: w 1893 oraz 1926 roku. Podczas II wojny światowej hitlerowcy doszczętnie zdewastowali budynek. Na początku synagogę zamienili w ujeżdżalnię koni, a później wykorzystywali jako magazyn. Po zakończeniu wojny przez wiele lat stała opuszczona i popadała w ruinę. W 1973 roku synagogę doszczętnie zniszczył pożar. W latach 1987-1991 została poddana kapitalnemu remontowi, wtedy planowano w budynku otworzyć kępińskie muzeum. Na podstawie ustawy z 1997 roku o restytucji mienia żydowskiego synagoga została zwrócona Żydowskiej Gminie Wyznaniowej we Wrocławiu. A następnie w 2009 roku gmina żydowska przekazała bóżnicę gminie Kępno.<br /><br />Obok Synagogi znajduje się Mykwa czyli żydowska łaźnia, z której przez długi okres czasu korzystali również nie żydowscy mieszkańcy Kępna."
+    );
+    mapa.dodaj(
+        [51.2800849, 17.9895302],
+        "Żydowski dom modlitwy (ulica Łazienkowa obok synagogi)",
+        "Został wybudowany w 1879 roku. Były to pomieszczenia przy synagodze, w których modlono się oraz studiowano Talmud. Dom modlitwy posiadał bibliotekę, ponadto już wtedy był oświetlany, natomiast zimą opalany. Z przekazów ustnych wiadomo , że od tamtej pory bóżnica otwierana była tylko w większe święta, natomiast na co dzień korzystano z domu modlitwy."
+    );
+    mapa.dodaj(
+        [51.28032, 17.98986],
+        "Żydowska jatka",
+        "Na rogu ulic  Strumykowej i E. Orzeszkowej znajduje się rytualna żydowska jatka czyli rzeźnia z przyległym obok żydowskim sklepem."
+    );  
+    mapa.dodaj(
+        [51.2828008, 17.9636539],
+        "Cmentarz żydowski (kirkut)",
+        "W poł. XIX wieku Kępno miało największą społeczność żydowską ze wszystkich miast  Księstwa Poznańskiego. W 1674 roku Żydzi, osiedliwszy się w Kępnie, otrzymali przywilej kolonizacyjny, który gwarantował im prawo założenia własnego cmentarza. Został on założony na tzw. Grzybowie, tą nazwą określano wówczas obszar obejmujący  końcową część ul. Wrocławskiej i początek Chojęcina. Od 1690 roku rozpoczęły się w Kępnie pierwsze pochówki. W 1909 roku obszar cmentarza obejmował prawie 2 hektary ziemi. Przed II wojną światową na cmentarzu znajdowało się około dwóch tysięcy macew wykonanych z marmuru, piaskowca i granitu. Cmentarz otoczonych był iglastymi drzewami, głównie sosnami. Podczas wojny Niemcy całkowicie zdewastowali teren cmentarza, wywożąc najcenniejsze macewy do III Rzeszy. Część z nich odnaleziono w trakcie remontu kępińskiego Rynku. Był nimi wyłożony basen przeciwpożarowy, który Niemcy zbudowali na początku wojny. Z kępińskich macew wzniesiono też Pomnik Bohaterów Walki o Wolność i Demokrację powstały w 1948 roku. Odnalezione macewy złożono obok synagogi. W 1959 roku cmentarz został przejęty przez skarb państwa. Obecnie na miejscu cmentarza żydowskiego znajduje się stacja kontroli pojazdów."
+    );
+    mapa.dodaj(
         [51.2800173, 17.9861782],
         "Szkoła żydowska",
         "Została wybudowana w 1837 roku przez gminę żydowską przy ulicy Szkolnej 6. W piętrowym budynku znajdowały się 4 klasy szkolne i oraz 2 mieszkania dla nauczycieli. W połowie XIX wieku uczyli tam nauczyciele: Hoffmann i Wieluner. Dzieci żydowskie uczyły się w niej do 1875 roku – do czasu zorganizowania wspólnej szkoły wyznaniowej."
@@ -14,6 +34,41 @@ $(document).ready(function() {
         [51.2799515, 17.9885432],
         "Dom Rabina Kępińskiej Gminy Żydowskiej",
         "Mieścił się w neoklasycystycznej kamienicy przy ulicy Warszawskiej 6. W tym budynku do 1939 roku mieszkał ostatni rabin Kępińskiej Gminy Żydowskiej."
+    );
+    mapa.dodaj(
+        [51.2763188, 17.9867604],
+        "Szpital żydowski i przytułek dla starców",
+        "Budynek znajdujący się przy dawnej ulicy Rychtalskiej 83, obecnie Sienkiewicza 22, został nabyty przez gminę żydowską w 1828 rok i przekształcony w szpital oraz przytułek dla starców. Kupno domu i prowadzenie szpitala umożliwił zapisem notarialnym Louis Schaps, kupiec z Berlina, którego korzenie wywodziły się z Kępna. Szpital przetrwał do wybuchu II wojny światowej. Obecnie budynek nie istnieje."
+    );
+    mapa.dodaj(
+        [51.2788778, 17.9848941],
+        "Kościół św. Marcina w Kępnie (ul. T. Kościuszki 3)",
+        "Początkowo była to drewniana świątynia powstała w 1685 roku. Jej fundatorem był starosta wieluński - Marcin Konstanty Olszowski, który podjął się tego daru po szczęśliwym powrocie z odsieczy wiedeńskiej. W 1830 roku ówczesny proboszcz - ks. Alojzy Grzybowski -  testamencie zapisał parafii kępińskiej folwark Grzybów, z dochodów którego miał być wybudowany nowy murowany kościół. Dzieła tego dokonał ks. proboszcz Ignacy Nowacki. Nowy kościół został wybudowany w latach 1909 – 1911, w stylu neobarokowym, a jego konsekracja nastapiła 15 X 1911 roku. Pierwowzorem budowli był kościół Trynitarzy w Krotoszynie z 1774 roku. Kępińska świątynia odróżnia się jednak oryginalną fasadą ozdobioną trzema figurami z piaskowca: Matki Bożej Wspomożycielki Wiernych, św. Wojciecha, św. Stanisława. Ołtarz główny zawiera obraz Najświętszego Serca Pana Jezusa autorstwa ucznia Jana Matejki i współautora Panoramy Racławickiej - Tadeusza Popiela. W 1960 roku kasetony na łukach sklepienia kościoła przyozdobione zostały  złoconymi rozetami. Ich fundatorem był długoletni proboszcz parafii pw. św. Marcina ks. prałat Marian Magnuszewski. Rozety stanowiły wotum za ocalenie z obozu koncentracyjnego w Dachau. W czasie okupacji niemieckiej świątynię zamknięto, okupant zorganizował w niej magazyn odzieży. Niemcy zarekwirowali dzwony i żelazny płot cmentarza przy świątyni."
+    );
+    mapa.dodaj(
+        [51.2715452, 17.9838402],
+        "Cmentarz katolicki (ul. Spółdzielcza)",
+        "Został założony w 1873 roku. Na uwagę zasługuje Grób Nieznanego Żołnierza, w którym zostali pochowani polscy żołnierze bohaterowie z czasów I wojny światowej oraz wojny polsko – bolszewickiej. W 1992 roku została wybudowana kaplica cmentarna pod wezwaniem Wszystkich Świętych."
+    );
+    mapa.dodaj(
+        [51.2760332, 17.9872942],
+        "Szkoła Podstawowa nr 1 (ul. Sienkiewicza 21)",
+        "Powstała w 1912 roku. Wówczas to władze Kępna postanowiły wybudować nowy gmach szkolny przy ulicy Rychtalskiej, obecnie Sienkiewicza 21. Plan budynku obejmował 20 izb lekcyjnych i salę gimnastyczną. Organizacją szkoły w wybudowanym gmachu zajął się jej pierwszy kierownik, Jan Błaszczak. Do tej szkoły uczęszczały dzieci różnych wyznań. Wybuch II wojny światowej spowodował upadek szkolnictwa. W czasie terroru okupanta w budynku utworzono szkołę podstawową dla dzieci niemieckich. Przez krótki okres czasu w szkole znajdował się szpital wojskowy oraz koszary."
+    );  
+    mapa.dodaj(
+        [51.2808924, 17.9860658],
+        "Kościół ewangelicko – augsburski w Kępnie (ul. Poniatowskiego 18)",
+        "Parafia ewangelicka w Kępnie istnieje od 2 grudnia 1661 roku.  W 1664 roku protestanci wybudowali w Kępnie dom modlitwy, czyli zbór. Pierwszym pastorem był Henryk Jamborowicz. W 1684 roku doszło do sporu między katolikami i ewangelikami. Orzeczeniem trybunału postanowiono zbór zamknąć. W obronie poszkodowanych wystąpił Marcin Olszowski. Po jego interwencji  dom modlitwy otwarto pod warunkiem, że protestanci obniżą dach swego budynku do poziomu domów mieszkalnych w mieście. W protestantów zostały wymierzone postanowienia Sejmu Niemego z 1717 roku, w wyniku których zniszczeniu miały ulec wszystkie zbory powstałe po 1632 roku. Zgodnie z tym orzeczeniem w dniach od 16 do 22 sierpnia 1718 roku zostały zburzone ewangelickie budowle: świątynia, plebania i szkoła. W 1774 roku sejm warszawski zatwierdził prawo gwarantujące wolność religijną. W 1776 roku uzyskano zgodę na budowę kościoła, który ukończono w 1779 roku. Podczas pożaru, który wybuchł w 1854  roku, spłonął zbór ewangelicki. Nowa świątynia - wzniesiona w 1863 roku w stylu neogotyckim -istnieje do dzisiaj.  Z okazji jubileuszu 250-lecia istnienia parafii świątynię gruntownie odnowiono."
+    );  
+    mapa.dodaj(
+        [51.2804857, 17.9863099],
+        "Szkoła ewangelicka (ul. Poniatowskiego 16)",
+        "Została wybudowana w 1857 roku przez Związek Szkolny Ewangelickiej Gminy Wyznaniowej. Parterowy budynek stanął przy ulicy Nowokościelnej, obecnie ul. Poniatowskiego 16. Jako samodzielna instytucja szkoła funkcjonowała do 1875 roku. Następnie przekazana została władzom Kępna. W 1911 roku budynek został odzyskany przez wyznaniową gminę ewangelicką i przekształcony w szpital oraz przytułek dla starców, który przetrwał do czasu II wojny światowej."
+    );   
+    mapa.dodaj(
+        [51.2707818, 17.9854305],
+        "Cmentarz ewangelicki (ul. Sienkiewicza 68)",
+        "Został założony w 1873 roku w czasie kiedy przestano korzystać ze starej nekropolii, znajdującej się we wschodniej części miasta. W 1895 roku została wzniesiona kaplica cmentarna. Sam cmentarz został powiększony  w 1900 roku. W ostatnich latach uległ dewastacji. W ruinę popadają klasycystyczne i neogotyckie nagrobki i grobowce. Od 2009 roku członkowie Stowarzyszenia „Socjum Kępno i Okolice” prowadzą prace porządkowe mające na celu zachowanie pięknej nekropolii oraz przywrócenie pamięci o ludziach tu pochowanych."
     ); 
         
     mapa.wyswietl();
